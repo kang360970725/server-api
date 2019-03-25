1b9d689c985fc19ef3e20ff16e109a1c25ba320f
Administrator <c_kang163@163.com>
2019-03-25 16:02:58 +0800

    2019/3/25  热门用户

--- business/dao/hotUsers.js
+++ business/dao/hotUsers.js
@@ -57,6 +57,61 @@ class dao {
             });
         })
     }
+
+
+    //查询热门用户榜单
+    static async getHotUserList(connection, query) {
+        let sql = () => `
+        SELECT
+            a.account,
+            a.uuid,
+            a.nickname,
+            c.bot_amount,
+            c.bot_lirun,
+            c.bot_prevDeposited,
+            c.bot_prevWithdrawn
+        FROM
+            users a
+        LEFT JOIN robot c ON a.account = c.user_account
+        WHERE
+            a.popular_user = 1 AND a.level < 5 AND c.bot_amount IS NOT NULL
+        ORDER BY
+            c.bot_amount DESC
+        LIMIT ?,?
+        `;
+        let params = [];
+        params.push((parseInt(query.index) - 1) * parseInt(query.pagesize));
+        params.push(parseInt(query.pagesize));
+        return new Promise(async (resolve, reject) => {
+            connection.query(sql(), params, (err, result) => {
+                if (err) return reject(err);
+                resolve(result);
+            });
+        })
+    }
+    //统计热门用户榜单总数
+    static async getHotUserListCount(connection, query) {
+        let sql = () => `
+        SELECT
+            count(1) as count
+        FROM
+            users a
+        LEFT JOIN robot c ON a.account = c.user_account
+        WHERE
+            a.popular_user = 1 AND a.level < 5 AND c.bot_amount IS NOT NULL
+        ORDER BY
+            c.bot_amount DESC
+        `;
+        let params = [];
+        params.push((parseInt(query.index) - 1) * parseInt(query.pagesize));
+        params.push(parseInt(query.pagesize));
+        return new Promise(async (resolve, reject) => {
+            connection.query(sql(), params, (err, result) => {
+                if (err) return reject(err);
+                resolve(result[0].count);
+            });
+        })
+    }
 }
 
 module.exports = dao
